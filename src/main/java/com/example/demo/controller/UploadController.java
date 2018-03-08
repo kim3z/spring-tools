2da7f1d68ca18668ba8f8cf9cc3826f807af4312
kim3z <kim.lehtinen@kubelex.com>
2018-03-08 18:32:44 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controller/UploadController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+public class UploadController {
+
+    private static String UPLOAD_DIR = System.getProperty("user.dir") + "\\upload-dir\\";
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_DIR + file.getOriginalFilename());
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Uploaded file: " + file.getOriginalFilename();
+    }
+
+}
